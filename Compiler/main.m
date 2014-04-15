ffa83522be74014ede0c8f96d9e82ca3387fa3eb
YuJianSirius <YuJianSirius@aol.com>
2014-04-15 20:40:25 +0800

    Initial Commit

---
+++ Compiler/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Compiler
+//
+//  Created by xiaojia on 4/15/14.
+//  Copyright (c) 2014 sirius. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
